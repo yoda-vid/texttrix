82a706a5071f7f370c67a09ff8841b005fbe8fe0
David Young <david@textflex.com>
2002-04-03 05:13:36 +0000

    *** empty log message ***

--- com/textflex/texttrix/TextTrix.java
+++ com/textflex/texttrix/TextTrix.java
@@ -1,21 +1,34 @@
 /*Text Trix
  *a goofy gui editor
- *v.0.1.1
- *Copyright (C) 2002 David Young
+ *v.0.1.3pre0
 
- *This program is free software; you can redistribute it and/or
- *modify it under the terms of the GNU General Public License
- *as published by the Free Software Foundation; either version 2
- *of the License, or (at your option) any later version.
+ *Copyright (c) 2002, David Young
+ *All rights reserved.
 
- *This program is distributed in the hope that it will be useful,
- *but WITHOUT ANY WARRANTY; without even the implied warranty of
- *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *GNU General Public License for more details.
+ *Redistribution and use in source and binary forms, with or without
+ *modification, are permitted provided that the following conditions 
+ *are met:
 
- *You should have received a copy of the GNU General Public License
- *along with this program; if not, write to the Free Software
- *Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Text Trix nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+ *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ *TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ *PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ *OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  */
 
@@ -25,38 +38,143 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.*;
 import java.awt.event.*;
+import java.io.*;
+import javax.swing.filechooser.FileFilter;
 
-/** The main <code>Edword</code> class.  Sets up the window
+/** The main <code>TextTrix</code> class.  Sets up the window
     and performs the text manipulation.
 */
 public class TextTrix extends JFrame {
-    JTextArea textArea = new JTextArea(30, 20);
-    JScrollPane scrollPane = new JScrollPane(textArea);
-    JButton stripReturns = new JButton("Remove Extra Hard Returns");
-    JMenuBar menuBar = new JMenuBar();
-    JMenu fileMenu = new JMenu("File");
-    JMenu editMenu = new JMenu("Edit");
-    JMenu helpMenu = new JMenu("Help");
+    ArrayList textAreas = new ArrayList();
+    JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+    JFileChooser chooser = new JFileChooser();
 
     public TextTrix() {
 	setTitle("Text Trix");
 	setSize(500, 600);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	Action exitAction = new
-	    AbstractAction("Exit") {
+	addTextArea(textAreas, tabbedPane);
+
+	JMenuBar menuBar = new JMenuBar();
+	JMenu fileMenu = new JMenu("File");
+	JMenu editMenu = new JMenu("Edit");
+	JMenu helpMenu = new JMenu("Help");
+
+	JMenuItem newItem = fileMenu.add(new AbstractAction("New") {
+		public void actionPerformed(ActionEvent evt) {
+		    addTextArea(textAreas, tabbedPane);
+		}
+	    });
+	newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,
+						      InputEvent.CTRL_MASK));
+
+	JMenuItem openItem = new JMenuItem("Open...");
+	fileMenu.add(openItem);
+	openItem.addActionListener(new FileOpenListener());
+	openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,
+						       InputEvent.CTRL_MASK));
+
+	JMenuItem closeItem = fileMenu.add(new AbstractAction("Close") {
+		public void actionPerformed(ActionEvent evt) {
+			int i = tabbedPane.getSelectedIndex();
+			TextPad t = (TextPad)textAreas.get(i);
+			if (t.getChanged()) {
+				String s = "Please save first.";
+				JOptionPane op = new JOptionPane();
+				op.showMessageDialog(op, s);
+			} else
+				removeTextArea(tabbedPane.getSelectedIndex(), textAreas, tabbedPane);
+		}
+	});
+
+	JMenuItem saveItem = new JMenuItem("Save as...");
+	fileMenu.add(saveItem);
+	saveItem.addActionListener(new FileSaveListener());
+	saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,
+						      InputEvent.CTRL_MASK));
+
+	JMenuItem exitItem = fileMenu.add(new AbstractAction("Exit") {
 		public void actionPerformed(ActionEvent evt) {
 		    System.exit(0);
 		}
-	    };
-	JMenuItem exitItem = fileMenu.add(exitAction);
+	    });
+	exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,
+						       InputEvent.CTRL_MASK));
 
-	textArea.setLineWrap(true);
-	textArea.setWrapStyleWord(true);
+	JMenuItem cutItem = editMenu.add(new AbstractAction("Cut") {
+		public void actionPerformed(ActionEvent evt) {
+		    ((TextPad)textAreas.get(tabbedPane.getSelectedIndex())).cut();
+		}
+	    });
+	cutItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
+						      InputEvent.CTRL_MASK));
 
+	JMenuItem copyItem = editMenu.add(new AbstractAction("Copy") {
+		public void actionPerformed(ActionEvent evt) {
+		    ((TextPad)textAreas.get(tabbedPane.getSelectedIndex())).copy();
+		}
+	    });
+	copyItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,
+						       InputEvent.CTRL_MASK));
+
+	JMenuItem pasteItem = editMenu.add(new AbstractAction("Paste") {
+		public void actionPerformed(ActionEvent evt) {
+		    ((TextPad)textAreas.get(tabbedPane.getSelectedIndex())).paste();
+		}
+	    });
+	pasteItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V,
+							InputEvent.CTRL_MASK));
+
+	editMenu.addSeparator();
+
+	JMenuItem selectAllItem = editMenu.add(new AbstractAction("Select All") {
+		public void actionPerformed(ActionEvent evt) {
+		    ((TextPad)textAreas.get(tabbedPane.getSelectedIndex())).selectAll();
+		}
+	    });
+	selectAllItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,
+							    InputEvent.CTRL_MASK));
+
+	JMenuItem aboutItem = helpMenu.add(new AbstractAction("About...") {
+		public void actionPerformed(ActionEvent evt) {
+			try {
+				BufferedReader reader = new BufferedReader(new
+					InputStreamReader(TextTrix.class.
+						getResourceAsStream("about.txt")));
+				String about = readText(reader);
+				reader.close();
+				JOptionPane op = new JOptionPane();
+				op.showMessageDialog(op, about);
+			} catch(IOException exception) {
+				exception.printStackTrace();
+			}
+		}
+	});
+
+	JMenuItem licenseItem = helpMenu.add(new AbstractAction("License") {
+		public void actionPerformed(ActionEvent evt) {
+			try {
+				BufferedReader reader = new BufferedReader(new
+					InputStreamReader(TextTrix.class.
+						getResourceAsStream("license.txt")));
+				String license = readText(reader);
+				reader.close();
+				addTextArea(textAreas, tabbedPane);
+				TextPad t = (TextPad)textAreas.get(tabbedPane.getSelectedIndex());
+				t.setEditable(false);
+				t.setText(license);
+			} catch(IOException exception) {
+				exception.printStackTrace();
+			}
+		}
+	});
+
+	JButton stripReturns = new JButton("Remove Extra Hard Returns");
 	stripReturns.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent evt) {
-		    textArea.setText(stripExtraHardReturns(textArea.getText()));
+		    TextPad t = (TextPad)textAreas.get(tabbedPane.getSelectedIndex());
+		    t.setText(t.stripExtraHardReturns(t.getText()));
 		}
 	    });
 
@@ -77,15 +195,62 @@ public class TextTrix extends JFrame {
 
 	constraints.fill = GridBagConstraints.BOTH;
 	constraints.anchor = GridBagConstraints.CENTER;
-	add(scrollPane, constraints, 0, 1, 1, 1, 100, 100);
+	add(tabbedPane, constraints, 0, 1, 1, 1, 100, 100);
 
     }
 
+    /**Publically executable starter method.
+       Creates the <code>TextTrix</code> object and displays it.
+     */
     public static void main(String[] args) {
 	TextTrix textTrix = new TextTrix();
 	textTrix.show();
     }
 
+	public String readText(BufferedReader reader) {
+		String text = "";
+		String line;
+		try {
+			while ((line = reader.readLine()) != null)
+				text = text + line + "\n";
+		} catch(IOException exception) {
+			exception.printStackTrace();
+		}
+		return text;
+	}
+
+    /**Creates a new <code>TextPad</code> object, a text area
+       for writing, and gives it a new tab.  Can call for
+       each new file; names the tab, <code>Filen.txt</code>,
+       where <code>n</code> is the tab number.
+    */
+    public void addTextArea(ArrayList arrayList, JTabbedPane tabbedPane) {
+		TextPad textPad = new TextPad(30, 20);
+		JScrollPane scrollPane = new JScrollPane(textPad);
+
+		textPad.setLineWrap(true);
+		textPad.setWrapStyleWord(true);
+		tabbedPane.addTab("NewFile.txt", scrollPane);
+		arrayList.add(textPad);
+		tabbedPane.setSelectedIndex(tabbedPane.getTabCount() - 1);
+	}
+	
+	public void removeTextArea(int i, ArrayList l, JTabbedPane tp) {
+		l.remove(i);
+		tp.remove(i);
+	}
+
+    /**Adds a new component to the <code>GridBagLayout</code>
+       manager.
+       @param c component to add
+       @param constraints layout constraints object
+       @param x column number
+       @param y row number
+       @param w number of columns to span
+       @param h number of rows to span
+       @param wx column weight
+       @param wy row weight
+    */
     public void add(Component c, GridBagConstraints constraints,
 			     int x, int y, int w, int h, 
 			     int wx, int wy) {
@@ -98,64 +263,104 @@ public class TextTrix extends JFrame {
 	getContentPane().add(c, constraints);
     }
 
-    /** Strips inserted, extra hard returns.  For example,
-	unformatted email arrives with hard returns
-	inserted after every line; this method
-	strips all but the paragraph, double-spaced
-	hard returns.  Text within <code>&#60pre&#62</code>
-	and <code>&#60/pre&#62</code> tags are
-	left untouched.
-	@param s the full text from which to strip
-	extra hard returns
-	@return stripped text
-    */
-    public String stripExtraHardReturns(String s) {
-	String stripped = "";
+    private class FileOpenListener implements ActionListener {
 
-	while (!s.equals("")) {
-	    int singleReturn = s.indexOf("\n");
-	    int doubleReturn = s.indexOf("\n\n");
-	    int dash = s.indexOf("-");
-	    int asterisk = s.indexOf("*");
-	    int startPre = s.indexOf("<pre>");
-	    int endPre = s.indexOf("</pre>");
+	public void actionPerformed(ActionEvent evt) {
+	    chooser.setCurrentDirectory(new File("."));
+	    final ExtensionFileFilter filter = new ExtensionFileFilter();
+	    filter.addExtension("txt");
+	    filter.setDescription("Text files (*.txt)");
+	    chooser.setFileFilter(filter);
 
-	    // don't catch internal dashes
-	    while (dash != -1 && dash < singleReturn) {
-		dash = s.indexOf("-", dash + 1);
-	    }
-	    while (asterisk != -1 && asterisk < singleReturn) {
-		asterisk = s.indexOf("*", asterisk + 1);
-	    }
+	    int result = chooser.showOpenDialog(TextTrix.this);
 
-	    if (startPre != -1 && startPre < singleReturn) {
-		if (endPre != -1) {
-		    stripped = stripped 
-			+ s.substring(0, endPre + 6);
-		    s = s.substring(endPre + 6);
-		} else {
-		    // if user forgets closing pre tag, goes to end
-		    stripped = stripped + s;
-		    s = "";
+	    if (result == JFileChooser.APPROVE_OPTION) {
+		String name = chooser.getSelectedFile().getPath();
+
+		try {
+		    BufferedReader reader = 
+				new BufferedReader(new FileReader(name));
+		    String text = readText(reader);
+			
+		    // check if tabs exist; get TextPad if true
+		    int tabIndex = tabbedPane.getSelectedIndex();
+			TextPad t = null;
+		    if (tabIndex != -1)
+				t = (TextPad)textAreas.get(tabIndex);
+		    // t.getText() != null, even if have typed nothing in it
+			// Add tab and set its text if no tabs exist or if current
+			// tab has tokens; set current tab's text otherwise
+		    if (tabIndex == -1
+					|| (new StringTokenizer(t.getText())).hasMoreTokens()) { 
+				addTextArea(textAreas, tabbedPane);
+				t = (TextPad)textAreas.get(tabbedPane.getSelectedIndex());
+				t.setText(text);
+		    } else {
+				t.setText(text);
+		    }
+			t.setChanged(false);
+
+		    reader.close();
+		} catch(IOException exception) {
+		    exception.printStackTrace();
 		}
-	    } else if (singleReturn == -1) {
-		stripped = stripped + s;
-		s = "";
-	    } else if (singleReturn == doubleReturn) {
-		stripped = stripped 
-		    + s.substring(0, doubleReturn + 2);
-		s = s.substring(doubleReturn +2);
-	    } else if (dash == singleReturn + 1
-		       || asterisk == singleReturn + 1) {
-		stripped = stripped 
-		    + s.substring(0, singleReturn + 2);
-		s = s.substring(singleReturn + 2);
-	    } else {
-		stripped = stripped 
-		    + s.substring(0, singleReturn) + " ";
-		s = s.substring(singleReturn + 1);
 	    }
 	}
-	return stripped;
+    }
+
+    private class FileSaveListener implements ActionListener {
+
+	public void actionPerformed(ActionEvent evt) {
+	    chooser.setCurrentDirectory(new File("."));
+	    final ExtensionFileFilter filter = new ExtensionFileFilter();
+	    filter.addExtension("txt");
+	    filter.setDescription("Text files (*.txt)");
+	    chooser.setFileFilter(filter);
+
+	    int result = chooser.showSaveDialog(TextTrix.this);
+
+	    if (result == JFileChooser.APPROVE_OPTION) {
+		String name = chooser.getSelectedFile().getPath();
+		try {
+			TextPad t = (TextPad)textAreas.get(tabbedPane.getSelectedIndex());
+			PrintWriter out = new PrintWriter(new FileWriter(name), true);
+		    out.print(t.getText());
+		    out.close();
+			t.setChanged(false);
+		} catch(IOException exception) {
+		    exception.printStackTrace();
+		}
+	    }
+	}
+    }
+
+}
+
+class ExtensionFileFilter extends FileFilter {
+    private String description = "";
+    private ArrayList extensions = new ArrayList();
+
+    public void addExtension(String extension) {
+	if (!extension.startsWith("."))
+	    extension = "." + extension;
+	extensions.add(extension.toLowerCase());
+    }
+
+    public void setDescription(String aDescription) {
+	description = aDescription;
+    }
+
+    public String getDescription() {
+	return description;
+    }
+
+    public boolean accept(File f) {
+	if (f.isDirectory()) return true;
+	String name = f.getName().toLowerCase();
+
+	for (int i = 0; i < extensions.size(); i++)
+	    if (name.endsWith((String)extensions.get(i)))
+		return true;
+	return false;
     }
 }
