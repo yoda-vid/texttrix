2035d405710486f77b98afd9dae85d9c33b1a789
David Young <david@textflex.com>
2003-05-20 08:26:11 +0000

    document builder script

---
+++ build-docs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Text Trix document builder
+# Copyright (c) 2003, Text Flex
+
+WK_DIR="/home/davit/src/texttrix"
+JAVA="/usr/java/j2sdk1.4.2/bin"
+
+cd $WK_DIR
+$JAVA/javadoc -d docs/api -link http://java.sun.com/j2se/1.4.2/docs/api com.textflex.texttrix
