0e3845496b816f79a6d7d8d56caa80de168870b3
David Young <david@textflex.com>
2004-12-29 23:49:08 +0000

    v.0.5.0rc1 release

--- com/textflex/texttrix/TextPad.java
+++ com/textflex/texttrix/TextPad.java
@@ -15,7 +15,7 @@
  *
  * The Initial Developer of the Original Code is
  * Text Flex.
- * Portions created by the Initial Developer are Copyright (C) 2002-4
+ * Portions created by the Initial Developer are Copyright (C) 2002-5
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s): David Young <dvd@textflex.com>
