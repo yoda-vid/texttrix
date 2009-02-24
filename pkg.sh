1a3675613a70a20ef33a4e928d9badc98eee70b4
David Young <david@textflex.com>
2009-02-24 02:02:36 +0000

    prep for 0.9.2 release

--- pkg.sh
+++ pkg.sh
@@ -16,7 +16,7 @@
 #
 # The Initial Developer of the Original Code is
 # Text Flex.
-# Portions created by the Initial Developer are Copyright (C) 2003-8
+# Portions created by the Initial Developer are Copyright (C) 2003-9
 # the Initial Developer. All Rights Reserved.
 #
 # Contributor(s): David Young <david@textflex.com>
